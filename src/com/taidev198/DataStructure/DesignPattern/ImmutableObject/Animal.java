0774946ef705625c2d5cdddac7ac6b18b30a6591
Nguyen Thanh Tai <traigiuxom9x@gmail.com>
2018-07-12 13:57:13 +0700

    create immutable object.

--- src/com/taidev198/DataStructure/DesignPattern/ImmutableObject/Animal.java
+++ src/com/taidev198/DataStructure/DesignPattern/ImmutableObject/Animal.java
@@ -1,5 +1,6 @@
 package com.taidev198.DataStructure.DesignPattern.ImmutableObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,12 +9,36 @@ import java.util.List;
  */
 public class Animal {
 
+    /**All fields are marked private and final,
+     * the constructor sets them upon object creation.
+     * There are no setter methods and the class its self marked final,
+     * so the methods cannot be overridden by a subclass.*/
     private  final List<String> favoriteFood;
+    private final String species;
+    private final int age;
+    public Animal(String species, int age, List<String> favoriteFood){
+        this.age = age;
+        this.species =species;
 
-    public Animal(List<String> favoriteFood){
+        //any mutable input arguments are copied to the instance instead of being
+        //used in directly.
         if (favoriteFood == null) throw  new RuntimeException("favoriteFood is required");
-        this.favoriteFood = favoriteFood;
+        this.favoriteFood = new ArrayList<>(favoriteFood);
     }
 
-    public List<String> getFavoriteFood(){ return favoriteFood;}//make class mutable
+    public int getAge() {
+        return age;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getFavoriteFood(int index) {
+        return favoriteFood.get(index);
+    }
+
+    public int getFavoriteFoodCount() {
+        return favoriteFood.size();
+    }
 }
